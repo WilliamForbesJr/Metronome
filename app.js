c9ba9b4b99c23da922f4c5bdc0690705d7da391b
Will <forbes8963@gmail.com>
2019-09-24 21:29:14 -0400

    style metronome to hide input. add input slider for tempo. Sync slider value with input field

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const button = document.querySelector("#start");
 const audio = document.querySelector('#click');
 const tempoInput = document.querySelector('#tempo');
+const metronome = document.querySelector('.metronome-wrapper')
+const slider = document.querySelector('.slider')
 
 let playing; //create global variable to avoid scoping issue with Setinterval()
 
@@ -9,6 +10,8 @@ const setClick = (tempo) => {
         if (!playing) {
             playing = setInterval(() => {
                 audio.play()
+                audio.currentTime = 0;
+                metronome.classList.add('click')
             }, tempo);
         } else {
             window.clearInterval(playing);
@@ -16,7 +19,23 @@ const setClick = (tempo) => {
         }
 }
 
-button.addEventListener("click", () => {
+const removeTransition = function (e) {
+    if (e.propertyName !== 'transform') return; //look for ONLY trasform properties
+    this.classList.remove('click')
+}
+
+const setTempo = () => {
     const tempo = 60000 / tempoInput.value //BPM to MS Conversion
     setClick(tempo);
-})
+}
+
+metronome.addEventListener("click", setTempo)
+
+metronome.addEventListener('transitionend', removeTransition)
+
+slider.addEventListener('change', function () {(tempoInput.value = this.value)})
+slider.addEventListener('mousemove', function () { (tempoInput.value = this.value) })
+
+
+tempoInput.addEventListener('change', () => slider.value = tempoInput.value)
+
