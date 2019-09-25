307be310c69977a899a729e29faff1f5dc2ad24a
Will <forbes8963@gmail.com>
2019-09-25 09:53:02 -0400

    refactor code to continue tempo when slider is adjusted. additional styling

--- app.js
+++ app.js
@@ -2,26 +2,23 @@ const audio = document.querySelector('#click');
 const tempoInput = document.querySelector('#tempo');
 const metronome = document.querySelector('.metronome-wrapper')
 const slider = document.querySelector('.slider')
-
+let slideChange = false;
 let playing; //create global variable to avoid scoping issue with Setinterval()
 
 
 const setClick = (tempo) => {
-        if (!playing) {
+        if (!playing || slideChange) {
+            window.clearInterval(playing)
             playing = setInterval(() => {
                 audio.play()
                 audio.currentTime = 0;
                 metronome.classList.add('click')
             }, tempo);
+            slideChange = false; //needs to stay false to reset slider value
         } else {
             window.clearInterval(playing);
             playing = null;
         }
-}
-
-const removeTransition = function (e) {
-    if (e.propertyName !== 'transform') return; //look for ONLY trasform properties
-    this.classList.remove('click')
 }
 
 const setTempo = () => {
@@ -29,13 +26,22 @@ const setTempo = () => {
     setClick(tempo);
 }
 
-metronome.addEventListener("click", setTempo)
+const removeTransition = function (e) {
+    if (e.propertyName !== 'transform') return; //look for ONLY trasform properties
+    this.classList.remove('click')
+}
+
+metronome.addEventListener("click", setTempo, slideChange = false)
 
 metronome.addEventListener('transitionend', removeTransition)
 
-slider.addEventListener('change', function () {(tempoInput.value = this.value)})
-slider.addEventListener('mousemove', function () { (tempoInput.value = this.value) })
+slider.addEventListener('change', function () {
+    tempoInput.value = this.value;
+    slideChange = !slideChange
+    setTempo();
+})
 
+slider.addEventListener('mousemove', function () { (tempoInput.value = this.value) })
 
 tempoInput.addEventListener('change', () => slider.value = tempoInput.value)
 
